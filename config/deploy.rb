cf5c55d6ffc4534750301d5ed20165000d507d42
Tim Camara <tim@desireepatno.com>
2010-09-14 13:30:03 -0700

    Third Commit - fixing deploy.rb and database.yml

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "cityserv"
+set :repository,  "git@github.com:tcamara/Cityserv.git"
+set :user, 'root'
+set :password, '0dysseus4'
+set :domain, 'cityserv.org'
+set :deploy_to, '/home/cityserv/public_html/cityserv.org/'
+set :use_sudo, false
+
+default_run_options[:pty] = true
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+role :db,  domain
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
